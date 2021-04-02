bf2183450c4129af6f8d28982caca2d89e1064a0
Aryan-Motwani <motwaniaryan7@gmail.com>
2021-04-02 22:17:27 +0530

    builded

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+// changes made
+
 import React, { Component } from "react";
 import { Route, withRouter } from "react-router-dom";
 import ListPage from "./ListPage";
-import Navbar from './Navbar'
-import SubCategory from './SubCategory'
+import Navbar from "./Navbar";
+import SubCategory from "./SubCategory";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div>
-        <Navbar/>
-        <Route exact path="/" component={ListPage}/>
-        <Route exact path="/:category" component={routerProps => <ListPage {...routerProps}/>}/>
-        <Route exact path="/a/:sub_category" component={routerProps => <SubCategory {...routerProps}/>}/>
+        <Navbar />
+        <Route exact path="/" component={ListPage} />
+        <Route
+          exact
+          path="/:category"
+          component={(routerProps) => <ListPage {...routerProps} />}
+        />
+        <Route
+          exact
+          path="/a/:sub_category"
+          component={(routerProps) => <SubCategory {...routerProps} />}
+        />
       </div>
     );
   }
